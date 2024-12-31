c11072d76f53324fe88d8e3d53b8bbe2195b0a6e
nawfalel1 <elachar2001@gmail.com>
2024-12-31 15:14:22 +0100

    adding logout

--- src/main/java/com/nelachar/sgi/auth/AuthenticationService.java
+++ src/main/java/com/nelachar/sgi/auth/AuthenticationService.java
@@ -1,6 +1,9 @@
 package com.nelachar.sgi.auth;
 
 
+import com.nelachar.sgi.token.Token;
+import com.nelachar.sgi.token.TokenRepository;
+import com.nelachar.sgi.token.TokenType;
 import com.nelachar.sgi.user.UserRepository;
 import com.nelachar.sgi.config.JwtService;
 import com.nelachar.sgi.user.Role;
@@ -19,6 +22,7 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final TokenRepository tokenRepository;
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
@@ -28,8 +32,9 @@ public class AuthenticationService {
                 .password(passwordEncoder.encode(request.getPassword()))
                 .role(Role.USER)
                 .build();
-        repository.save(user);
+        var savedUser = repository.save(user);
         var jwtToken = jwtService.generateToken(user);
+        saveUserToken(savedUser, jwtToken);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
@@ -45,8 +50,33 @@ public class AuthenticationService {
         );
         var user = repository.findByUsername(request.getUsername()).orElseThrow();
         var jwtToken = jwtService.generateToken(user);
+        revokeAllUserTokens(user);
+        saveUserToken(user,jwtToken);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
+    }
+
+    private void revokeAllUserTokens(User user){
+        var validUserTokens = tokenRepository.findAllValidTokensByUser(user.getId());
+        if(validUserTokens.isEmpty()){
+            return;}
+        validUserTokens.forEach(t -> {
+            t.setExpired(true);
+            t.setRevoked(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
+
+
+    }
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .revoked(false)
+                .expired(false)
+                .build();
+        tokenRepository.save(token);
     }
 }
---
+++ src/main/java/com/nelachar/sgi/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.nelachar.sgi.token;
+
+public enum TokenType {
+    BEARER
+}
